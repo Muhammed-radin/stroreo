ad152f0bc1959f6e40b0876140fa687b88b23352
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-04-11 17:43:58 +0530

    view page model created

--- view/view.js
+++ view/view.js
@@ -1,4 +1,13 @@
 var nowUrl = window.location.href;
+
+if (location.search == '' || location.search == false) {
+  //document.getElementById('no_id').style.display = 'block'
+  //document.getElementById('body').style.display = 'none'
+} else {
+  document.getElementById('no_id').style.display = 'none'
+  // document.getElementById('body').style.display = 'block'
+}
+
 var params = location.search.replace('?', '');
 var app = params.replace('app=', '')
 console.log(app);
