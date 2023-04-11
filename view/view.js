889bebc8a817ebb2719eae94d07a3dab1870879e
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-04-11 18:09:03 +0530

    view page disabled content enabled

--- view/view.js
+++ view/view.js
@@ -1,11 +1,11 @@
 var nowUrl = window.location.href;
 
 if (location.search == '' || location.search == false) {
-  //document.getElementById('no_id').style.display = 'block'
-  //document.getElementById('body').style.display = 'none'
+  document.getElementById('no_id').style.display = 'block'
+  document.getElementById('body').style.display = 'none'
 } else {
   document.getElementById('no_id').style.display = 'none'
-  // document.getElementById('body').style.display = 'block'
+  document.getElementById('body').style.display = 'block'
 }
 
 var params = location.search.replace('?', '');
